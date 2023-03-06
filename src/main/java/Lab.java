f1d896a412641fa9117d995e2a2f3660c0dd19fc
Meri Galstyan <meriegalstyan@gmail.com>
2023-03-06 01:01:06 +0000

    ComparatorsTest: Complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,3 +1,4 @@
+import org.hamcrest.core.IsEqual;
 
 /**
  * If we want to make programs that "do something", we're going to need to create instructions that change
@@ -23,49 +24,71 @@ public class Lab {
      * @param b the second number to be compared.
      * @return true if a is equal to b, false otherwise.
      */
-    public boolean isEqual(int a, int b){
+    public boolean isEqual(int a, int b){if (a==b) {
+        return true;
+    } else {
         return false;
     }
+    };
 
     /**
      * @param a the first number to be compared.
      * @param b the second number to be compared.
      * @return true if a is greater than b, false otherwise.
      */
-    public boolean greaterThan(int a, int b){
+    public boolean greaterThan(int a, int b){if (a>b) {
+        return true;
+    } else {
         return false;
-    }
+        
+    }};
     /**
      * @param a the first number to be compared.
      * @param b the second number to be compared.
      * @return true if a is less than b, false otherwise.
      */
-    public boolean lessThan(int a, int b){
-        return false;
+    public boolean lessThan(int a, int b){if(a<b) {
+        return true;
+    } else {return false;
     }
+    };
     /**
      * @param a the first number to be compared.
      * @param b the second number to be compared.
      * @return true if a is greater than or equal to b, false otherwise.
      */
-    public boolean greaterThanOrEqualTo(int a, int b){
+    public boolean greaterThanOrEqualTo(int a, int b){if (a>=b) {
+        return true;
+    }
+    else {
         return false;
     }
+    };
     /**
      * @param a the first number to be compared.
      * @param b the second number to be compared.
      * @return true if a is less than or equal to b, false otherwise.
      */
-    public boolean lessThanOrEqualTo(int a, int b){
+    public boolean lessThanOrEqualTo(int a, int b){if (a<=b) {
+        return true;
+    }
+    else {
         return false;
     }
+};
 
     /**
      * @param a the first number to be compared.
      * @param b the second number to be compared.
      * @return true if a is not equal to b, false otherwise.
      */
-    public boolean notEqualTo(int a, int b){
+    public boolean notEqualTo(int a, int b){if (a!=b) {
+        return true;
+    }
+    else {
         return false;
     }
+};
 }
+
+
